318eb67ada467357cf8a37a11672d8f3e68e5646
Henry Ang <guohaoang@berkeley.edu>
2022-06-05 16:55:49 -0700

    fix bug about loading history

--- src/main/java/edu/cs244b/chat/event/EventHandler.java
+++ src/main/java/edu/cs244b/chat/event/EventHandler.java
@@ -41,11 +41,15 @@ public class EventHandler implements IEventHandler {
 
 	@Override
 	public MessageRequest analyzeMessage(List<MessageContext> messageContext, String userId) {
-		// ReceiveMessages
+		System.out.println("Event handler received messages. Number of msgs: " + messageContext.size() + ".");
+		for (MessageContext msg : messageContext) {
+			System.out.println(msg);
+		}
+		// Divide incoming messages by room id
 		HashMap<String, List<MessageContext>> messagesByRoomId = new HashMap<>();
 		for (MessageContext message : messageContext) {
 			String roomId = message.getRoomId();
-			if (!messageContext.contains(roomId)) {
+			if (!messagesByRoomId.containsKey(roomId)) {
 				messagesByRoomId.put(roomId, new ArrayList<>());
 			}
 			List<MessageContext> messages = messagesByRoomId.get(roomId);
@@ -95,12 +99,13 @@ public class EventHandler implements IEventHandler {
 						event.eventId, event.timestamp, event.depth, event.content));
 			}
 		}
+		Collections.sort(messageContexts);
 		return messageContexts;
 	}
 
 	@Override
 	public List<MessageContext> getAllMessagesFromRoom(String roomId) {
-		LinkedList<MessageContext> messageContexts = new LinkedList<>();
+		ArrayList<MessageContext> messageContexts = new ArrayList<>();
 		Room room = rooms.get(roomId);
 		System.out.println(room.eventGraph.events.size());
 		for (Event event: room.eventGraph.events.values()) {
@@ -112,21 +117,9 @@ public class EventHandler implements IEventHandler {
 			messageContexts.add(new MessageContext(roomId, new ArrayList<>(room.userIds), event.senderId, new ArrayList<>(event.parentEventIds),
 					event.eventId, event.timestamp, event.depth, event.content));
 		}
+		Collections.sort(messageContexts);
 		return messageContexts;
 	}
-
-	public List<MessageContext> ConvertAllRoomsToMessageContexts() {
-        LinkedList<MessageContext> messageContexts = new LinkedList<>();
-        for (String roomId : rooms.keySet()) {
-            Room room = rooms.get(roomId);
-            for (Event event: room.eventGraph.events.values()) {
-                messageContexts.add(new MessageContext(roomId, new ArrayList<>(room.userIds), event.senderId, new ArrayList<>(event.parentEventIds),
-                        event.eventId, event.timestamp, event.depth, event.content));
-            }
-        }
-        return messageContexts;
-    }
-
 
 	// The new_message argument is supposed to have the timestamp, owner_id, message content and the room id.
 	public List<MessageContext> HandleNewMessageInternal(MessageContext new_message) {
@@ -147,6 +140,7 @@ public class EventHandler implements IEventHandler {
     public HashMap<String, Room> rooms;
 
 	private void AddMessagesToEventGraph(EventGraph eventGraph, List<MessageContext> messages) {
+		System.out.println(messages);
 		HashMap<String, Event> newEvents = MessagesToEvents(messages);
 		List<Event> eventsSortedByDepth = new ArrayList<>(newEvents.values());
 		eventsSortedByDepth.sort(Comparator.comparingInt(e -> e.depth));
